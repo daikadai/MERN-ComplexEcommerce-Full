68885f6c6113157332076ac9eb7f13bcf01e88c4
Pham Cao Dai <daikadai1026@gmail.com>
2021-01-05 16:22:43 +0700

    3. Protected User Route

---
+++ client/src/pages/user/History.js
@@ -0,0 +1,14 @@
+import React from 'react'
+
+const History = () => {
+  return (
+    <div className='container-fluid'>
+      <div className="row">
+        <div className="col">user history page</div>
+      </div>
+    </div>
+  )
+}
+
+export default History
+ 
