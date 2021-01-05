93e49d81c7507e1949d46c89558cabd97c37ab00
Pham Cao Dai <daikadai1026@gmail.com>
2021-01-05 16:46:57 +0700

    5. Sidebar Nav for User

--- client/src/pages/auth/Login.js
+++ client/src/pages/auth/Login.js
@@ -8,8 +8,8 @@ import { Link } from "react-router-dom";
 import { createOrUpdateUser } from "../../functions/auth";
 
 const Login = ({ history }) => {
-  const [email, setEmail] = useState("daikadai96@gmail.com");
-  const [password, setPassword] = useState("123456");
+  const [email, setEmail] = useState("daikadai1026@gmail.com");
+  const [password, setPassword] = useState("123123");
   const [loading, setLoading] = useState(false);
 
   const { user } = useSelector((state) => ({ ...state }));
