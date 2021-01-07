9e7f65ca306c70f1b1ce13b5b1ff7531a59c3f84
Pham Cao Dai <daikadai1026@gmail.com>
2021-01-07 16:48:24 +0700

    15. Category Create and List

--- client/src/pages/admin/category/CategoryCreate.js
+++ client/src/pages/admin/category/CategoryCreate.js
@@ -1,17 +1,76 @@
-import React from 'react'
-import AdminNav from '../../../components/nav/AdminNav'
+import React, { useState, useEffect } from "react";
+import { toast } from "react-toastify";
+import AdminNav from "../../../components/nav/AdminNav";
+import { useSelector } from "react-redux";
+import {
+  createCategory,
+  getCategories,
+  removeCategory,
+} from "../../../functions/category";
 
 const CategoryCreate = () => {
+  const [name, setName] = useState("");
+  const [loading, setLoading] = useState(false)
+  const [categories, setCategories] = useState([])
+
+  const { user } = useSelector(state => state)
+  
+  useEffect(() => { 
+    loadCategories()
+  },[])
+
+  const loadCategories = () => getCategories().then(c => setCategories(c.data))
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    setLoading(true)
+    createCategory({ name }, user.token)
+      .then(res => {
+        setLoading(false)
+        setName('')
+        toast.success(`"${res.data.name}" is created`)
+      })
+      .catch(err => {
+        console.log(err.response.data);
+        setLoading(false)
+        if(err.response.status === 400)
+        toast.error(err.response.data)
+      })
+  };
+
+  const categoryForm = () => (
+    <form onSubmit={handleSubmit}>
+      <div className="form-group">
+        <label>Name</label>
+        <input
+          type="text"
+          className="form-control"
+          value={name}
+          onChange={(e) => setName(e.target.value)}
+          autoFocus
+          required
+        />
+        <br/>
+        <button className='btn btn-outline-primary'>Save</button>
+      </div>
+    </form>
+  );
+
   return (
     <div className="container-fluid">
       <div className="row">
         <div className="col-md-2">
           <AdminNav />
         </div>
-        <div className="col">Category Create Page</div>
+        <div className="col">
+          {loading ? <h4 className='text-danger'>Loading...</h4> : <h4>Create category</h4>}
+          {categoryForm()}
+          <hr/>
+          {categories.length}
+        </div>
       </div>
     </div>
-  )
-}
+  );
+};
 
-export default CategoryCreate
+export default CategoryCreate;
