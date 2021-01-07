d71da95f5dc77392c89a5614314b6f24d7bf7829
Pham Cao Dai <daikadai1026@gmail.com>
2021-01-07 16:11:13 +0700

    14. Dynamic Dashboard Link && Admin Sidebar Nav

--- client/src/App.js
+++ client/src/App.js
@@ -19,6 +19,7 @@ import Password from "./pages/user/Password";
 import Wishlist from "./pages/user/Wishlist";
 import AdminDashboard from "./pages/admin/AdminDashboard";
 import AdminRoute from "./components/routes/AdminRoute";
+import CategoryCreate from "./pages/admin/category/CategoryCreate";
 
 const App = () => {
   const dispatch = useDispatch();
@@ -64,6 +65,7 @@ const App = () => {
         <UserRoute exact path="/user/password" component={Password} />
         <UserRoute exact path="/user/wishlist" component={Wishlist} />
         <AdminRoute exact path="/admin/dashboard" component={AdminDashboard} />
+        <AdminRoute exact path="/admin/category" component={CategoryCreate} />
       </Switch>
     </>
   );
--- client/src/functions/category.js
+++ client/src/functions/category.js
@@ -5,3 +5,39 @@ export const getCategories = async () => {
     `${process.env.REACT_APP_API}/categories`
   );
 };
+
+export const getCategory = async (slug) => {
+  return await axios.get(
+    `${process.env.REACT_APP_API}/category/${slug}`
+  );
+};
+
+export const removeCategory = async (slug, authtoken) => {
+  return await axios.delete(
+    `${process.env.REACT_APP_API}/category/${slug}`, {
+      headers: {
+        authtoken
+      }
+    }
+  );
+};
+
+export const udpateCategory = async (slug, category, authtoken) => {
+  return await axios.put(
+    `${process.env.REACT_APP_API}/category/${slug}`, {
+      headers: {
+        authtoken
+      }
+    }
+  );
+};
+
+export const createCategory = async (category, authtoken) => {
+  return await axios.post(
+    `${process.env.REACT_APP_API}/category`, category, {
+      headers: {
+        authtoken
+      }
+    }
+  );
+};
