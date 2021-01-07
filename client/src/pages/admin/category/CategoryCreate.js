a69184c7c3838b1acfaeb63626cace08a5e7f289
Pham Cao Dai <daikadai1026@gmail.com>
2021-01-07 17:17:50 +0700

    16. Category Render and Delete

--- client/src/pages/admin/category/CategoryCreate.js
+++ client/src/pages/admin/category/CategoryCreate.js
@@ -7,36 +7,58 @@ import {
   getCategories,
   removeCategory,
 } from "../../../functions/category";
+import { Link } from "react-router-dom";
+import { DeleteOutlined, EditOutlined } from "@ant-design/icons";
 
 const CategoryCreate = () => {
   const [name, setName] = useState("");
-  const [loading, setLoading] = useState(false)
-  const [categories, setCategories] = useState([])
+  const [loading, setLoading] = useState(false);
+  const [categories, setCategories] = useState([]);
 
-  const { user } = useSelector(state => state)
-  
-  useEffect(() => { 
-    loadCategories()
-  },[])
+  const { user } = useSelector((state) => state);
 
-  const loadCategories = () => getCategories().then(c => setCategories(c.data))
+  useEffect(() => {
+    loadCategories();
+  }, []);
+
+  const loadCategories = () =>
+    getCategories().then((c) => setCategories(c.data));
 
   const handleSubmit = (e) => {
     e.preventDefault();
-    setLoading(true)
+    setLoading(true);
     createCategory({ name }, user.token)
+      .then((res) => {
+        setLoading(false);
+        setName("");
+        toast.success(`"${res.data.name}" is created`);
+        loadCategories()
+      })
+      .catch((err) => {
+        console.log(err.response.data);
+        setLoading(false);
+        if (err.response.status === 400) toast.error(err.response.data);
+      });
+  };
+
+  const handleRemove = async (slug) => {
+    if(window.confirm('Delete?')) {
+      setLoading(true)
+      removeCategory(slug, user.token)
       .then(res => {
         setLoading(false)
-        setName('')
-        toast.success(`"${res.data.name}" is created`)
+        toast.error(`${res.data.name} deleted`)
+        loadCategories()
       })
       .catch(err => {
-        console.log(err.response.data);
-        setLoading(false)
-        if(err.response.status === 400)
-        toast.error(err.response.data)
+        if (err.response.status === 400) {
+          setLoading(false);
+          toast.error(err.response.data);
+        }
       })
-  };
+    }
+
+  }
 
   const categoryForm = () => (
     <form onSubmit={handleSubmit}>
@@ -50,8 +72,8 @@ const CategoryCreate = () => {
           autoFocus
           required
         />
-        <br/>
-        <button className='btn btn-outline-primary'>Save</button>
+        <br />
+        <button className="btn btn-outline-primary">Save</button>
       </div>
     </form>
   );
@@ -63,10 +85,29 @@ const CategoryCreate = () => {
           <AdminNav />
         </div>
         <div className="col">
-          {loading ? <h4 className='text-danger'>Loading...</h4> : <h4>Create category</h4>}
+          {loading ? (
+            <h4 className="text-danger">Loading...</h4>
+          ) : (
+            <h4>Create category</h4>
+          )}
           {categoryForm()}
-          <hr/>
-          {categories.length}
+          <hr />
+          {categories.map((c) => (
+            <div key={c._id} className="alert alert-secondary">
+              {c.name}{" "}
+              <span
+                onClick={() => handleRemove(c.slug)}
+                className="btn btn-sm float-right"
+              >
+                <DeleteOutlined className="text-danger" />
+              </span>{" "}
+              <Link to={`/admin/category/${c.slug}`}>
+                <span className="btn btn-sm float-right">
+                  <EditOutlined className="text-warning" />
+                </span>
+              </Link>
+            </div>
+          ))}
         </div>
       </div>
     </div>
