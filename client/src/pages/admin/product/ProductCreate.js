c8df824102c22f7bfff6311b6fdf0d87b041a78f
Pham Cao Dai <daikadai1026@gmail.com>
2021-02-20 18:05:51 +0700

    34. Code Refactor

--- client/src/pages/admin/product/ProductCreate.js
+++ client/src/pages/admin/product/ProductCreate.js
@@ -3,6 +3,7 @@ import { toast } from "react-toastify";
 import { useSelector } from "react-redux";
 import AdminNav from "../../../components/nav/AdminNav";
 import { createProduct } from "../../../functions/product";
+import ProductCreateForm from "../../../components/forms/ProductCreateForm";
 
 const initialState = {
   title: "Macbook Pro",
@@ -24,21 +25,6 @@ const ProductCreate = () => {
   const [values, setValues] = useState(initialState);
 
   const { user } = useSelector(state => ({...state}))
-  const {
-    title,
-    description,
-    price,
-    categories,
-    category,
-    subs,
-    shipping,
-    quantity,
-    images,
-    colors,
-    brands,
-    color,
-    brand,
-  } = values;
 
   const handleSubmit = (e) => {
     e.preventDefault()
@@ -70,86 +56,11 @@ const ProductCreate = () => {
           <h4>Product create</h4>
           <hr />
 
-          <form onSubmit={handleSubmit}>
-            <div className="form-group">
-              <label>Title</label>
-              <input
-                type="text"
-                name="title"
-                className="form-control"
-                value={title}
-                onChange={handleChange}
-              />
-            </div>
-
-            <div className="form-group">
-              <label>Description</label>
-              <input
-                type="text"
-                name="description"
-                className="form-control"
-                value={description}
-                onChange={handleChange}
-              />
-            </div>
-
-            <div className="form-group">
-              <label>Price</label>
-              <input
-                type="text"
-                name="price"
-                className="form-control"
-                value={price}
-                onChange={handleChange}
-              />
-            </div>
-
-            <div className="form-group">
-              <label>Shipping</label>
-              <select name="shipping"  className='form-control' onChange={handleChange}>
-                <option>Please select</option>
-                <option value='No'>No</option>
-                <option value='Yes'>Yes</option>
-              </select>
-            </div>
-
-            <div className="form-group">
-              <label>Quantity</label>
-              <input
-                type="text"
-                name="quantity"
-                className="form-control"
-                value={quantity}
-                onChange={handleChange}
-              />
-            </div>
-
-            <div className="form-group">
-              <label>Color</label>
-              <select name="color"  className='form-control' onChange={handleChange}>
-                <option>Please select</option>
-                {
-                  colors.map(c => (
-                    <option key={c} value={c}>{c}</option>
-                  ))
-                }
-              </select>
-            </div>
-
-            <div className="form-group">
-              <label>Brand</label>
-              <select name="brand"  className='form-control' onChange={handleChange}>
-                <option>Please select</option>
-                {
-                  brands.map(b => (
-                    <option key={b} value={b}>{b}</option>
-                  ))
-                }
-              </select>
-            </div>
-
-            <button className="btn btn-outline-info">Save</button>
-          </form>
+          <ProductCreateForm 
+            handleSubmit={handleSubmit}
+            handleChange={handleChange}
+            values={values}
+          />
         </div>
       </div>
     </div>
