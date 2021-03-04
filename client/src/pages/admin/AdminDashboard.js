a14603cf0a48a788686e04f30bb3f943a1be82e6
Pham Cao Dai <daikadai1026@gmail.com>
2021-03-04 15:46:59 +0700

    48. Fetch All Products in Admin Dashboard

--- client/src/pages/admin/AdminDashboard.js
+++ client/src/pages/admin/AdminDashboard.js
@@ -1,17 +1,42 @@
-import React from 'react'
-import AdminNav from '../../components/nav/AdminNav'
+import React, { useEffect, useState } from "react";
+import AdminNav from "../../components/nav/AdminNav";
+import { getProductsByCount } from "../../functions/product";
 
 const AdminDashboard = () => {
+  const [products, setProducts] = useState([]);
+  const [loading, setLoading] = useState(false);
+
+  useEffect(() => {
+    loadAllProducts();
+  }, []);
+
+  const loadAllProducts = () => {
+    setLoading(true);
+    getProductsByCount(100)
+      .then((res) => {
+        setProducts(res.data);
+        setLoading(false);
+      })
+      .catch((err) => {
+        setLoading(false);
+        console.log(err)
+      });
+  };
   return (
     <div className="container-fluid">
       <div className="row">
         <div className="col-md-2">
           <AdminNav />
         </div>
-        <div className="col">Admin Dashboard Page</div>
+        { loading ? (
+          <h4 className='text-danger'>Loading...</h4>
+        ) : (
+          <h4>All Products</h4>
+        )}
+        <div className="col">{JSON.stringify(products)}</div>
       </div>
     </div>
-  )
-}
+  );
+};
 
-export default AdminDashboard
+export default AdminDashboard;
