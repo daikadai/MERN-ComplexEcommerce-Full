40e266d4fd6234ab21de1a11ad8c64140e228446
Pham Cao Dai <daikadai1026@gmail.com>
2021-03-04 16:09:05 +0700

    49. Admin Products Card

---
+++ client/src/components/cards/AdminProductCard.js
@@ -0,0 +1,23 @@
+import { Card } from "antd";
+import React from "react";
+
+const { Meta } = Card;
+const AdminProductCard = ({ product }) => {
+  const { title, description, images } = product;
+
+  return (
+    <Card
+      cover={
+        <img
+          src={images && images.length ? images[0].url : ""}
+          style={{ height: "150px", objectFit: "cover" }}
+          className="m-2"
+        />
+      }
+    >
+      <Meta title={title} description={description} />
+    </Card>
+  );
+};
+
+export default AdminProductCard;
--- client/src/pages/admin/AdminDashboard.js
+++ client/src/pages/admin/AdminDashboard.js
@@ -1,4 +1,5 @@
 import React, { useEffect, useState } from "react";
+import AdminProductCard from "../../components/cards/AdminProductCard";
 import AdminNav from "../../components/nav/AdminNav";
 import { getProductsByCount } from "../../functions/product";
 
@@ -19,7 +20,7 @@ const AdminDashboard = () => {
       })
       .catch((err) => {
         setLoading(false);
-        console.log(err)
+        console.log(err);
       });
   };
   return (
@@ -28,12 +29,20 @@ const AdminDashboard = () => {
         <div className="col-md-2">
           <AdminNav />
         </div>
-        { loading ? (
-          <h4 className='text-danger'>Loading...</h4>
-        ) : (
-          <h4>All Products</h4>
-        )}
-        <div className="col">{JSON.stringify(products)}</div>
+        <div className="col">
+          {loading ? (
+            <h4 className="text-danger">Loading...</h4>
+          ) : (
+            <h4>All Products</h4>
+          )}
+          <div className="row">
+            {products.map((product) => (
+              <div key={product._id} className="col-md-4">
+                <AdminProductCard product={product} />
+              </div>
+            ))}
+          </div>
+        </div>
       </div>
     </div>
   );
